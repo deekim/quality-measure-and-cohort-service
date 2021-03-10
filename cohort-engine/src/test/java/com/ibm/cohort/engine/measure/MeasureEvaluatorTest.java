968ae5cfa62fff3c6a3cb355ff5e088b827bb445
David Kwasny <dkwasny@us.ibm.com>
2021-03-10 15:22:03 -0500

    Finishing another round of API refactoring

--- cohort-engine/src/test/java/com/ibm/cohort/engine/measure/MeasureEvaluatorTest.java
+++ cohort-engine/src/test/java/com/ibm/cohort/engine/measure/MeasureEvaluatorTest.java
@@ -31,14 +31,10 @@ import org.hl7.fhir.r4.model.Patient;
 import org.hl7.fhir.r4.model.RelatedArtifact;
 import org.junit.Before;
 import org.junit.Test;
-import org.opencds.cqf.common.evaluation.EvaluationProviderFactory;
 import org.opencds.cqf.common.evaluation.MeasurePopulationType;
 
 import com.ibm.cohort.engine.LibraryFormat;
 import com.ibm.cohort.engine.measure.evidence.MeasureEvidenceOptions;
-import org.opencds.cqf.common.providers.LibraryResolutionProvider;
-import org.opencds.cqf.cql.engine.data.DataProvider;
-import org.opencds.cqf.cql.engine.terminology.TerminologyProvider;
 
 public class MeasureEvaluatorTest extends BaseMeasureTest {
 
@@ -50,15 +46,12 @@ public class MeasureEvaluatorTest extends BaseMeasureTest {
 	public void setUp() {
 		super.setUp();
 
-		// TODO: Clean up with some helper something or another
-		MeasureResolutionProvider<Measure> measureProvider = new RestFhirMeasureResolutionProvider(client);
-		LibraryResolutionProvider<Library> libraryProvider = new RestFhirLibraryResolutionProvider(client);
-		EvaluationProviderFactory factory = new ProviderFactory(client, client, null);
-		TerminologyProvider terminologyProvider = factory.createTerminologyProvider(null,null,null,null,null);
-		Map<String, DataProvider> dataProviders = new HashMap<>();
-		dataProviders.put("http://hl7.org/fhir", factory.createDataProvider(null, null));
-
-		evaluator = new MeasureEvaluator(measureProvider, libraryProvider, terminologyProvider, dataProviders);
+		FHIRClientContext clientContext = new FHIRClientContext.Builder()
+				.withDefaultClient(client)
+				.build();
+		evaluator = new R4MeasureEvaluatorBuilder()
+				.withClientContext(clientContext)
+				.build();
 	}
 
 	@Test
